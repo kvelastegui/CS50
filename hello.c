018b4bd64d04ea97446e5c60c77697287aab9422
kvelastegui <51062341+kvelastegui@users.noreply.github.com>
2020-11-23 21:32:59 -0700

    Delete hello.c

--- hello.c
+++
@@ -1,9 +0,0 @@
-#include <cs50.h>
-#include <stdio.h>
-
-//get name from user and say hello
-int main(void)
-{
-    string name = get_string("What is your name?\n");
-    printf("Hello, %s\n", name);
-}
